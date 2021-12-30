fb48f5d875615cd42fb706164edd4cb1d8389e17
Matthew Rodriguez <mr359@njit.edu>
2021-12-30 16:01:04 -0500

    first

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import Accordion from './components/Accordion';
+import Search from './components/Search';
+import Dropdown from './components/Dropdown';
+import Translate from './components/Translate';
+import Header from './components/Header';
+import Route from './components/Route';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const items = [
+  {
+    title: "What is React?",
+    content: "React is a front end framework" 
+  },
+  {
+    title: "Why use React?",
+    content: "React is a favorite JS library among engineers" 
+  },
+  {
+    title: "How do you use React?",
+    content: "You use React by creating componets" 
+  }
+];
+
+const options = [
+  {
+    label: "The color Red",
+    valuew: "red"
+  },
+  {
+    label: "The color Green",
+    valuew: "green"
+  },
+  {
+    label: "A shade of blue",
+    valuew: "blue"
+  }
+];
+
+const App = () => {
+  const [selected, setSelected] = useState(options[0]);
+
+  return(
+    <div>
+      <Header/>
+      <Route path="/">
+        <Accordion items={items}/>
+      </Route>
+      <Route path="/list">
+        <Search/>
+      </Route>
+      <Route path="/dropdown">
+        <Dropdown 
+          label="Select a Color"
+          selected={selected}
+          onSelectedChanged={setSelected}
+          options={options}
+        />
+      </Route>
+      <Route path="/translate">
+        <Translate/>
+      </Route>
     </div>
-  );
+  )
 }
 
 export default App;
